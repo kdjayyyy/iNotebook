f374efb3a3cb4407cd4c8050a5adb2e19a679aef
Kinjal Das <thekdas1000@gmail.com>
2023-12-31 20:02:49 +0530

    api testing for the user login done

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,14 @@
 const connectToMongo = require('./db');
-
 const express = require('express');
+const authRouter = require('./routes/auth');
+
 const app = express();
-const port = 3000;
+const port = 5000;
 connectToMongo();
 app.use(express.json());
 
 // AVAILABLE ROUTES
-app.use('/api/auth', require('./routes/auth'));
+app.use('/api/auth', authRouter);
 // app.use('/api/notes', require('./routes/notes'));
 
 app.listen(port, () => {
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,21 +1,41 @@
 const express = require('express');
-const User = require('../models/User')
+const User = require('../models/User');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
-// create a user using: POST "/api/auth". doesn't require auth
+// create a user using: POST "/api/auth/createUser". doesn't require auth
 
-// const newUser = new User({
-//   name: 'Ravi',
-//   email: 'ravi@gmail.com',
-//   password: 'arucoMarkers',
-// });
+router.post('/createUser', [
+  body('name').isLength({ min: 3 }),
+  body('email').isEmail(),
+  body('password').isLength({ min: 5 })
+], async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-router.get('/', (req, res) => {
+    // Check if the user with the provided email already exists
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res.status(400).json({ error: 'User with this email already exists!' });
+    }
 
+    // Create a new user
+    user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    });
 
-
-  res.send(req.body);
+    // Return the newly created user
+    return res.json(user);
+  } 
+  catch(err) {
+    console.error(`Error: ${err.message}`);
+    return res.status(500).json({ error: 'Server Error' });
+  }
 });
 
 module.exports = router;
-           
